24bdf7d81705b9e734f219b3a4ce86d61ed152af
vermouth <vermouth_fee@outlook.com>
2020-02-15 19:58:36 +0800

    feat: add fibonacci max and min index Of fibonacci numbers

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,5 +1,7 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.math.BigInteger;
+
 /**
  *
  *
@@ -11,20 +13,26 @@ public class Fibonacci {
         this.indexLimit = indexLimit;
     }
 
-    public Integer getFibonacciNumber(Integer index) {
+    public Long getFibonacciNumber(long index) {
         if (index == 1 || index == 2) {
-            return 1;
+            return Long.valueOf(1);
         }
         return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 
     public String getFibonacci() {
         StringBuilder fibonacciString = new StringBuilder();
+
+        if(this.indexLimit < 1) {
+            return String.valueOf('0');
+        }
+
         for (int i = 1; i <= this.indexLimit; i++) {
             fibonacciString.append(getFibonacciNumber(i).toString() + ',');
         }
         fibonacciString.deleteCharAt(fibonacciString.length() -1);
 
+        System.out.println(fibonacciString);
         return fibonacciString.toString();
     }
 
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -3,13 +3,17 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 import cc.xpbootcamp.warmup.cashier.OrderReceipt;
 import cc.xpbootcamp.warmup.fibonacci.Fibonacci;
+import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
 class FibonacciTest {
     @Test
@@ -32,10 +36,29 @@ class FibonacciTest {
 
     @Test
     void shouldReturnFirstFiveFibonacciSplitWithCommaSeparated() {
-        Fibonacci fibonacci = new Fibonacci(5);
+        Fibonacci fibonacci = new Fibonacci(50);
 
         String fibonacciString = fibonacci.getFibonacci();
 
         assertThat(fibonacciString, containsString("1,1,2,3,5"));
     }
+    //TODO how to compare two long number
+//
+//    @Test
+//    void shouldGetNumber0WhenIndexIs0() {
+//        Fibonacci fibonacci = new Fibonacci(5);
+//
+//        Long fibonacciNumber = fibonacci.getFibonacciNumber(0);
+//
+//        assertEquals(java.util.Optional.of(0), fibonacciNumber);
+//    }
+
+//    @Test
+//    void shouldReturnFiftiethFibonacciWithNumber12586269025() {
+//        Fibonacci fibonacci = new Fibonacci(50);
+//
+//        BigInteger fibonacciNumber = fibonacci.getFibonacciNumber(50);
+//
+//        Assert.isTrue(fibonacciNumber.subtract(BigInteger.valueOf(12586269025)) == 0);
+//    }
 }
